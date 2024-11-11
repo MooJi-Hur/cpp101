6a6b45290a452b4c84b519c2d2a72be918c4fb49
MooJi <mooji.hur@gmail.com>
2024-11-11 12:45:36 +0900

    [Feature] add overriding

--- src/lecture_04_inheritance/main.cpp
+++ src/lecture_04_inheritance/main.cpp
@@ -32,6 +32,10 @@ class Tea {
     int getWater() { return water; }
 
     void showPrice() { cout << "The total price : " << price * count << endl; }
+
+    // price 멤버 변수 자체는 private 처리를 했기 때문에, 단일 price 가격을 받아오기 위한 getter
+    // 함수를 정의함
+    int getUnitPrice() { return price; }
 };
 
 // c++은 다중 상속이 가능함
@@ -57,22 +61,37 @@ class BlackTea : public Tea, private IngredientUnit {
         cout << "The total sugar : " << getCount() * getUnitSugar() * sugarLevel << endl;
         cout << "The total water : " << getCount() * getUnitWater() * getWater() << endl;
     }
+
+    // 오버라이딩 overriding : 덮어쓰기
+    // 기존 부모 클래스의 멤버 함수를 (무시하고) 덮었씀
+    // 같은 showPrice 이름이지만 단위 가격을 출력하도록 재정의
+    void showPrice() { cout << "The BlackTea Price : " << getUnitPrice() << endl; }
 };
 
 int main(void) {
+    // 물 2, 갯수 3으로 Tea 인스턴스 생성
+    Tea originTea = Tea(2, 3);
+
+    cout << "Count : " << originTea.getCount() << endl;      // Count : 3
+    cout << "Price : " << originTea.getUnitPrice() << endl;  // Price : 3000
+
+    // 부모인 Tea 클래스에서 showPrice 함수는 전체 가격을 출력
+    originTea.showPrice();  // The total price : 9000
+
     // BlackTea의 인스턴스인 firstOrder 생성
     // 클래스 자체는 힙이나 스택에 할당되지 않으며 인스턴스를 생성해야 객체의 멤버가 메모리에 할당됨
     // water = 2, count = 3, sugarLevel = 2;
     // 인스턴스 생성 과정은 src/lecture_04_inheritance/README.md 참조
     BlackTea firstOrder = BlackTea(2, 3, 2);
 
-    firstOrder.showPrice();  // The total price : 9000
-
     cout << "Current value of water : " << firstOrder.getWater()
          << endl;  // Current value of water : 2
 
     firstOrder.showSugarLevel();   // Current sugar level : 2
     firstOrder.showIngredients();  // The total sugar : 60 // The total water : 600
+
+    // 자식 BlackTea 클래스에서 showPrice는 홍차 한 잔의 가격을 출력
+    firstOrder.showPrice();  // The BlackTea Price : 3000
 
     return 0;
 }
